5647ed45a27e689b51407609023fdec5364f5655
bibekanandan892 <48781831+bibekanandan892@users.noreply.github.com>
2021-08-16 00:26:06 +0530

    Add files via upload

---
+++ application_logging/logger.py
@@ -0,0 +1,21 @@
+# =============================================================================
+# # ---------------------------------------------------------------------------
+# # Creation Date: 25-jun-2021
+# # Author: Bibekananda Nayak
+# # Version: 1.0
+# #
+# # ---------------------------------------------------------------------------#
+# =============================================================================
+rom datetime import datetime
+
+
+class App_Logger:
+    def __init__(self):
+        pass
+
+    def log(self, file_object, log_message):
+        self.now = datetime.now()
+        self.date = self.now.date()
+        self.current_time = self.now.strftime("%H:%M:%S")
+        file_object.write(
+            str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message +"\n")
